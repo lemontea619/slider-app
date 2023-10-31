1692d0eac630613d367c94cf8b3cf9cd984cf1aa
lemontea619 <rain_619@icloud.com>
2023-10-31 16:32:10 +0900

    cssまで

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("test");
+const listElement = document.getElementById("list");
+console.log(listElement);
+// listElement.scrollLeft = 1000;
+// listElement.style.transform = "translateX(-500px)";
