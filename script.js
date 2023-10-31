681761aa45e0ceb12cd2c94073b415036a7e4c51
lemontea619 <rain_619@icloud.com>
2023-10-31 16:50:53 +0900

    clickイベント作成

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log("test");
 const listElement = document.getElementById("list");
-console.log(listElement);
+
 // listElement.scrollLeft = 1000;
 // listElement.style.transform = "translateX(-500px)";
+
+const li = document.querySelector("li");
+console.log(li);
+
+function clickRightButton() {
+  listElement.scrollLeft += li.scrollWidth;
+  console.log("clickRightButton");
+}
+
+const clickLeftButton = () => {
+  listElement.scrollLeft -= li.scrollWidth;
+  console.log("clickLeftButton");
+};
